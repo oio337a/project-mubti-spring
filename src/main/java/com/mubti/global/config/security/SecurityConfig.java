8bfecc11eb10225d1e853a11a2529699301abccd
zerone015 <zerone015@naver.com>
2022-06-03 21:21:51 +0900

    style : 주석 추가

--- src/main/java/com/mubti/global/config/security/SecurityConfig.java
+++ src/main/java/com/mubti/global/config/security/SecurityConfig.java
@@ -41,39 +41,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                    .cors()
+                .cors() // cors 설정.
                 .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement() // 세션 관리 매니저 생성
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않는다. (JWT 방식을 사용하기 위한 설정)
                 .and()
-                    .csrf().disable()
-                    .formLogin().disable()
-                    .httpBasic().disable()
-                    .exceptionHandling()
-                    .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-                    .accessDeniedHandler(tokenAccessDeniedHandler)
+                .csrf().disable() // rest api를 이용한 서버는 session 기반 인증과는 다르게 stateless하기 때문에 서버에 인증 정보를 보관하지 않으므로 불필요한 csrf 코드 제거
+                .formLogin().disable() // 토큰 방식으로 인증처리를 하므로 form login을 사용하지 않는다.
+                .httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
+                .exceptionHandling() // 예외 처리 기능 작동
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint()) // 인증 실패 처리를 설정한다.
+                .accessDeniedHandler(tokenAccessDeniedHandler) // 접근 거부 때 처리 할 핸들러를 설정한다.
                 .and()
-                    .authorizeRequests()
-                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                    .antMatchers("/api/**").hasAnyAuthority(RoleType.USER.getCode())
-                    .antMatchers("/api/**/admin/**").hasAnyAuthority(RoleType.ADMIN.getCode())
-                    .anyRequest().authenticated()
+                .authorizeRequests() // requestMatchers를 구현하여 요청의 접근을 제한할 수 있다.
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Preflight 요청은 모두 허용한다.
+                .antMatchers("/api/**").hasAnyAuthority(RoleType.USER.getCode()) // USER 권한을 가지는 사용자만 접근 가능
+                .antMatchers("/api/**/admin/**").hasAnyAuthority(RoleType.ADMIN.getCode()) // ADMIN 권한을 가지는 사용자만 접근 가능
+                .anyRequest().authenticated() // 나머지 요청은 전부 인증 필요
                 .and()
-                    .oauth2Login()
-                    .authorizationEndpoint()
-                    .baseUri("/oauth2/authorization")
-                    .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                .oauth2Login() // oauth2.0 설정
+                .authorizationEndpoint() // 인가 엔드포인트 설정
+                .baseUri("/oauth2/authorization") // 인가 엔드포인트 설정
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()) // 인가 요청 정보 저장소 설정
                 .and()
-                    .redirectionEndpoint()
-                    .baseUri("/*/oauth2/code/*")
+                .redirectionEndpoint() // 인가 응답 엔드포인트 설정
+                .baseUri("/*/oauth2/code/*") // 인가 응답 엔드포인트 설정
                 .and()
-                    .userInfoEndpoint()
-                    .userService(oAuth2UserService)
+                .userInfoEndpoint() // 유저 정보 엔드포인트 설정
+                .userService(oAuth2UserService) // 유저 정보 서비스 설정
                 .and()
-                    .successHandler(oAuth2AuthenticationSuccessHandler())
-                    .failureHandler(oAuth2AuthenticationFailureHandler());
+                .successHandler(oAuth2AuthenticationSuccessHandler())
+                .failureHandler(oAuth2AuthenticationFailureHandler()); // 인증 성공, 실패 핸들러 설정
 
-        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // 커스텀 필터 등록. UsernamePasswordAuthenticationFilter 보다 더 먼저 실행
     }
 
     /*
