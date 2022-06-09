91fb06180ce7e216ad0eae6f4ac6cea41ce0ff3e
oio337a <oio337a@gmail.com>
2022-06-10 00:02:20 +0900

    chore : 파일 동기화를 위한 커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Home from "./components/Home";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.js";
 import Redirect from "./pages/redirect.js";
 import Test from "./pages/test.js";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-            <Route path="/" element={<h1>hello</h1>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/oauth/redirect" element={<Redirect />} />
-            <Route path="/test" element={<Test />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>hello</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/oauth/redirect" element={<Redirect />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
