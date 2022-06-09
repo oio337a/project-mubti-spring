a9f5a44e62961cdcfebbbc006e0ce468101bdf53
zerone015 <zerone015@naver.com>
2022-06-09 19:10:41 +0900

    Fix : 응답 쿼리스트링 토큰 만료기간 오류 수정

--- src/main/java/com/mubti/global/common/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/mubti/global/common/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -106,7 +106,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
         // 리다이렉트 uri에 쿼리스트링으로 액세스 토큰 값을 응답해준다.
         return UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("token", accessToken.getToken(), "expiry", expiry)
+                .queryParam("token", accessToken.getToken())
+                .queryParam("expiryTime", expiry)
                 .build().toUriString();
     }
 
