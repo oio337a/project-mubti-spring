499b5071cca9d7dac05be0ebed455dafa001e255
SOONG-E <sksuwjd@naver.com>
2022-06-11 22:43:13 +0900

    postsService 수정

--- frontend/src/service/PostsService.js
+++ frontend/src/service/PostsService.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import api from "../utils/api";
+import {useSelector} from "react-redux";
 
-const BOARD_API_BASE_URL = "http://localhost:8080/api/v1/posts";
+const BOARD_API_BASE_URL = "http://localhost:8080/posts";
 
 class PostsService {
     getBoards() {
-        return api.get(BOARD_API_BASE_URL);
+        const token = useSelector((state) => state.user.value);
+        return axios.get(BOARD_API_BASE_URL, {headers: {Authorization: `Bearer ${token.accessToken}`}});
     }
 }
 
