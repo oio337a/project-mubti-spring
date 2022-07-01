c7aa7c539a2a5038e8153b9b612f1412af5133be
zerone015 <zerone015@naver.com>
2022-07-01 15:41:23 +0900

    Fix : 게시글 조회 정렬, 검색 오류 수정

--- src/main/java/com/mubti/domain/post/service/impl/PostServiceImpl.java
+++ src/main/java/com/mubti/domain/post/service/impl/PostServiceImpl.java
@@ -29,7 +29,24 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Page<PostResponseDto> getPostList(Pageable pageable, String categoryType, String searchType, String keyword){
-        Page<Post> postList = postRepository.selectPostLIst(pageable, categoryType, searchType, keyword);
+        Page<Post> postList = null;
+
+        switch(searchType) {
+            case "title" :
+                postList = postRepository.selectPostListByTitle(pageable, categoryType, keyword);
+                break;
+            case "content" :
+                postList = postRepository.selectPostListByContent(pageable, categoryType, keyword);
+                break;
+            case "userAlias" :
+                postList = postRepository.selectPostListByUserAlias(pageable, categoryType, keyword);
+                break;
+            case "title_content" :
+                postList = postRepository.selectPostListByTitleContent(pageable, categoryType, keyword);
+                break;
+            default :
+                postList = postRepository.findAll(pageable);
+        }
 
         return postList.map(post -> new PostResponseDto(post));
     }
