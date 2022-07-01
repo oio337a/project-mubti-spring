c58b830f1447d0c0eaea6f6df6fad850a215532a
zerone015 <zerone015@naver.com>
2022-07-01 18:47:04 +0900

    Fix : 에러 수정

--- src/main/java/com/mubti/domain/post/repository/PostRepository.java
+++ src/main/java/com/mubti/domain/post/repository/PostRepository.java
@@ -10,16 +10,28 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(value = "SELECT * FROM post WHERE post_category LIKE %:categoryType% AND post_title LIKE %:keyword%", nativeQuery = true)
+    @Query(
+            value = "SELECT * FROM post WHERE post_category LIKE %:categoryType% AND post_title LIKE %:keyword%",
+            countQuery = "SELECT count(*) FROM post",
+            nativeQuery = true)
     Page<Post> selectPostListByTitle(Pageable pageable, @Param("categoryType") String categoryType, @Param("keyword") String keyword);
 
-    @Query(value = "SELECT * FROM post WHERE post_category LIKE %:categoryType% AND post_content LIKE %:keyword%", nativeQuery = true)
+    @Query(
+            value = "SELECT * FROM post WHERE post_category LIKE %:categoryType% AND post_content LIKE %:keyword%",
+            countQuery = "SELECT count(*) FROM post",
+            nativeQuery = true)
     Page<Post> selectPostListByContent(Pageable pageable, @Param("categoryType") String categoryType, @Param("keyword") String keyword);
 
-    @Query(value = "SELECT p.* FROM post p INNER JOIN user u ON p.user_seq = u.user_seq WHERE post_category LIKE %:categoryType% AND u.user_alias LIKE %:keyword%", nativeQuery = true)
+    @Query(
+            value = "SELECT p.* FROM post p INNER JOIN user u ON p.user_seq = u.user_seq WHERE post_category LIKE %:categoryType% AND u.user_alias LIKE %:keyword%",
+            countQuery = "SELECT count(*) FROM post",
+            nativeQuery = true)
     Page<Post> selectPostListByUserAlias(Pageable pageable, @Param("categoryType") String categoryType, @Param("keyword") String keyword);
 
-    @Query(value = "SELECT * FROM Post WHERE post_category LIKE %:categoryType% AND (post_title LIKE %:keyword% OR post_content LIKE %:keyword%)", nativeQuery = true)
+    @Query(
+            value = "SELECT * FROM Post WHERE post_category LIKE %:categoryType% AND (post_title LIKE %:keyword% OR post_content LIKE %:keyword%)",
+            countQuery = "SELECT count(*) FROM post",
+            nativeQuery = true)
     Page<Post> selectPostListByTitleContent(Pageable pageable, @Param("categoryType") String categoryType, @Param("keyword") String keyword);
 
 }
